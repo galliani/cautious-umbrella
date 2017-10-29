8f2e5154460f6a4a01870d6d0c5cc8d0cb82ce6b
Galih Muhammad <galih0muhammad@gmail.com>
2017-10-29 17:30:41 +0700

    fix major bug in decide and advance command

--- lib/manage_applicant.rb
+++ lib/manage_applicant.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 module ManageApplicant
   # Setting up variables and constants
   HIRED_DECISION_BIN = 1
@@ -19,58 +20,54 @@ module ManageApplicant
 
   def self.advance_stage(inputs, defined_stages, given_command)
     email = inputs.first
-    input_stage = inputs.size > 1 ? nil : inputs.last
+    input_stage = inputs.size >= 2 ? inputs.last : nil 
 
     current_stage = @@applicants[email]
-    is_last_stage = current_stage == defined_stages.keys.last
+    if current_stage == 1 || current_stage == 0
+      return "The application of #{email} has been decided already"
+    end
 
     # Check first if the applicant is already in the last stage
-    if is_last_stage
-      "Already in #{current_stage}"
+    is_last_stage = current_stage == defined_stages.keys.last
+    return "Already in #{current_stage}" if is_last_stage
+      
+    # Check if the STAGE_NAME argument is nil or not,
+    # if nil, then just advance 1 stage ahead.
+    if input_stage.nil?
+      current_stage_index = defined_stages[current_stage]
+
+      next_stage_index = current_stage_index + 1
+      next_stage = defined_stages.key(next_stage_index)
+      
+      # Move the stage for the applicants
+      @@applicants[email] = next_stage
     else
-      # Check if the STAGE_NAME argument is nil or not,
-      # if nil, then just advance 1 stage ahead.
-      if input_stage.nil?
-        current_stage_index = defined_stages[current_stage]
+      is_same_stage = current_stage == input_stage
+      return "Already in #{current_stage}" if is_same_stage
 
-        next_stage_index = current_stage_index + 1
-        next_stage = defined_stages.key(next_stage_index)
-        
-        # Move the stage for the applicants
-        @@applicants[email] = next_stage
-
-        given_command + ' ' + next_stage
-      else
-        is_same_stage = current_stage == input_stage
-        return "Already in #{current_stage}" if is_same_stage
-
-        # Move the stage for the applicants
-        @@applicants[email] = next_stage
-
-        given_command + ' ' + input_stage
-      end        
+      # Move the stage for the applicants
+      @@applicants[email] = input_stage
     end
+
+    given_command + ' ' + email
   end
 
   def self.make_decision(inputs, last_stage)
     email = inputs.first
-    decision_input = inputs.size > 2 ? nil : inputs.last
+    decision_input = inputs.size >= 2 ? inputs.last.to_i : nil
     return "Failed to decide for #{email}" if decision_input.nil?
-
-    # Set current_stage for the applicant to nil to indicate that decision has been made
-    @@applicants[email] = nil
 
     # HIRING decision rules
     is_to_be_hired = decision_input == HIRED_DECISION_BIN
-    is_hireable = @@applicants[email] == last_stage
+    
+    if is_to_be_hired
+      is_hireable = @@applicants[email] == last_stage
+      return self.finish_application(:hired, email) if is_hireable
 
-    if is_to_be_hired && is_hireable
-      self.increment_counter(:hired)
-      "Hired #{email}"
+      "Failed to decide for #{email}"
+    else
+      self.finish_application(:rejected, email)
     end
-
-    self.increment_counter(:rejected)
-    "Rejected #{email}"
   end
 
   def self.gathering_stats(defined_stages)
@@ -92,7 +89,11 @@ module ManageApplicant
 
   private
 
-  def self.increment_counter(counter_sym)
+  def self.finish_application(counter_sym, email)
     @@decision_counter[counter_sym] += 1
+    @@applicants[email] = counter_sym == :hired ? 1 : 0
+
+    return "Hired #{email}" if counter_sym == :hired
+    "Rejected #{email}"
   end
 end
