bb1bcaed30044aea36fd6c59aa0a5075a3352972
Galih Muhammad <galih0muhammad@gmail.com>
2017-10-29 16:35:22 +0700

    refactor again

--- lib/process_command.rb
+++ lib/process_command.rb
@@ -2,44 +2,33 @@ module ProcessCommand
   # Setting up variables and constants
   WHITELISTED_COMMANDS = ['DEFINE', 'CREATE', 'ADVANCE', 'DECIDE', 'STATS']
   @@defined_stages = {}
-  @@line_index = 0
 
-  def self.execute(full_inputs, output)
-    given_command = full_inputs.first
-
+  def self.execute(given_command, inputs, output)
     if WHITELISTED_COMMANDS.include?(given_command)
-      
-      inputs = full_inputs.drop(1)
 
-
-      if given_command == 'DEFINE'
-        
-        inputs.each_with_index { |e,i| @@defined_stages[e] = i }
-        newline = given_command + ' ' + @@defined_stages.keys.join(' ')
-        
-      elsif given_command == 'CREATE'
-
+      case given_command
+      when 'DEFINE'        
+        newline = self.defining_stages(given_command, inputs)
+      when 'CREATE'
         newline = ManageApplicant.register(inputs.first, @@defined_stages.keys.first)      
-
-      elsif given_command == 'ADVANCE'
-
-        newline = ManageApplicant.advance_stage(
-          inputs, @@defined_stages, given_command
-        )      
-      
-      elsif given_command == 'DECIDE'
-
-        newline = ManageApplicant.make_decision(inputs, @@defined_stages.keys.last)      
-
-      elsif given_command == 'STATS'
-
-        newline = ManageApplicant.gathering_stats(@@defined_stages)      
-
+      when 'ADVANCE'
+        newline = ManageApplicant.advance_stage(inputs, @@defined_stages, given_command) 
+      when 'DECIDE'
+        newline = ManageApplicant.make_decision(inputs, @@defined_stages.keys.last)
+      when 'STATS'
+        newline = ManageApplicant.gathering_stats(@@defined_stages)
       end
 
       output.puts(newline)
     end
+  end
 
-    @@line_index += 1
+  private
+
+  def self.defining_stages(given_command, inputs)
+    inputs.each_with_index { |e,i| @@defined_stages[e] = i }
+    stages_stringified = @@defined_stages.keys.join(' ')
+
+    given_command + ' ' + stages_stringified
   end
 end
--- text_reader.rb
+++ text_reader.rb
@@ -2,16 +2,27 @@ require_relative 'lib/process_command'
 require_relative 'lib/manage_applicant'
 
 filename = ARGV.first.nil? ? 'input.txt' : ARGV.first
-puts "Operation is starting, your file input is #{filename}"
+puts "Operation starts, your file input is #{filename}"
 
 output_name = 'output.txt'
 output = File.new(output_name, "w")
 
+line_index = 0
+
 File.foreach(filename) do |line|
   full_inputs = line.split(' ')
-  
-  ProcessCommand.execute(full_inputs, output)
+  given_command = full_inputs.first
+  inputs = full_inputs.drop(1)
+
+  if given_command != 'DEFINE' && line_index.zero?
+    puts 'Operation terminated early, please define hiring stages first'
+    break 
+  end
+
+  ProcessCommand.execute(given_command, inputs, output)
+
+  line_index += 1
 end
 
 output.close
-puts "Operation went without a problem, please check #{output_name}"
+puts "Operation is done"
